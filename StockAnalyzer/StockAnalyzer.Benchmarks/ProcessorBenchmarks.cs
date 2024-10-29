aa28adb95092dffb018c7b5d98bf055a150150db
Thomas GUILLIER <thomas.guillier@sogeti.com>
2024-10-29 16:29:41 +0100

    Exercice 3 : Ecrire un benchmark avec BenchmarDotNet.

---
+++ StockAnalyzer/StockAnalyzer.Benchmarks/ProcessorBenchmarks.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace StockAnalyzer.Benchmarks
+{
+    [SimpleJob]
+    [MemoryDiagnoser]
+    public class ProcessorBenchmarks
+    {
+        [GlobalSetup]
+        public void Setup() { }
+
+        [Benchmark]
+        public List<string> Processor()
+        {
+            var result = new List<string>();
+
+            var processor = new Processor.Processor("D:\\source\\repos\\Perf_Exercices\\StockAnalyzer\\StockAnalyzer.Processor\\Data");
+
+            processor.Initialize();
+
+            foreach (var stock in processor.Stocks)
+            {
+                var min = processor.Min(stock.Key);
+                var max = processor.Max(stock.Key);
+                var average = processor.Average(stock.Key);
+
+                result.Add($"{min} {max} {average}");
+            }
+
+            return result;
+        }
+    }
+}
