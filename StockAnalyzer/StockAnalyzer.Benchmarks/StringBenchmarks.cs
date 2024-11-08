11aa6026ab9532c33f8634290ec7b942a2da44a9
Thomas GUILLIER <thomas.guillier@sogeti.com>
2024-11-08 16:15:01 +0100

    Exercice 13 : Benchmark string operations.

---
+++ StockAnalyzer/StockAnalyzer.Benchmarks/StringBenchmarks.cs
@@ -0,0 +1,52 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using System.Text;
+
+namespace StockAnalyzer.Benchmarks
+{
+    [SimpleJob(RunStrategy.ColdStart, iterationCount: 5)]
+    [MemoryDiagnoser]
+    public class StringBenchmarks
+    {
+        [Benchmark]
+        public string BuildString()
+        {
+            string result = "";
+
+            for (int i = 0; i < 50000; i++)
+            {
+                result += i;
+                result += Environment.NewLine;
+            }
+
+            return result;
+        }
+
+        [Benchmark]
+        public string BuildStringInterpolation()
+        {
+            string result = "";
+
+            for (int i = 0; i < 50000; i++)
+            {
+                result = $"{result}{i}{Environment.NewLine}";
+            }
+
+            return result;
+        }
+
+        [Benchmark]
+        public string BuildStringBuilder()
+        {
+            var builder = new StringBuilder();
+
+            for (int i = 0; i < 50000; i++)
+            {
+                builder.Append(i);
+                builder.Append(Environment.NewLine);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
