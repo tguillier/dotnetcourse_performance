a8489ac12a82d54d4489a3c44b4423779540a412
Thomas GUILLIER <thomas.guillier@sogeti.com>
2024-11-08 17:48:56 +0100

    Exercice 15 : Benchmark exceptions for flow control.

---
+++ StockAnalyzer/StockAnalyzer.Benchmarks/FlowControlBenchmarks.cs
@@ -0,0 +1,61 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+
+namespace StockAnalyzer.Benchmarks
+{
+    [SimpleJob(RunStrategy.ColdStart, iterationCount: 5)]
+    [MemoryDiagnoser]
+    public class FlowControlBenchmarks
+    {
+        [Benchmark]
+        public decimal ProcessWithException()
+        {
+            decimal result = 0;
+
+            for (int i = 0; i < 10000; i++)
+            {
+                try
+                {
+                    var dimensions = GetDimensionsFor(i);
+                    result += dimensions.Width;
+                }
+                catch
+                {
+                    // Do nothing.
+                }
+            }
+
+            return result;
+        }
+
+        [Benchmark]
+        public decimal ProcessWithoutException()
+        {
+            decimal result = 0;
+
+            for (int i = 0; i < 10000; i++)
+            {
+                var dimensions = GetDimensionsFor(i);
+
+                if (dimensions is not null)
+                {
+                    result += dimensions.Width;
+                }
+            }
+
+            return result;
+        }
+
+        public Dimensions GetDimensionsFor(int index)
+        {
+            if (index % 10 == 0)
+            {
+                return null!;
+            }
+
+            return new(100, 100);
+        }
+    }
+
+    public record Dimensions(int Width, int Height);
+}
