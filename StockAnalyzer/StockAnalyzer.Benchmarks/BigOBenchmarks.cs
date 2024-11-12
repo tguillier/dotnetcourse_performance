8d4cb37899459d8146939326462b6b0e10fc3656
Thomas GUILLIER <thomas.guillier@sogeti.com>
2024-11-12 15:25:01 +0100

    Exercice 16 : Compare algorithm complexity.

---
+++ StockAnalyzer/StockAnalyzer.Benchmarks/BigOBenchmarks.cs
@@ -0,0 +1,72 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using StockAnalyzer.Processor;
+using System.Collections.Immutable;
+
+namespace StockAnalyzer.Benchmarks
+{
+    [SimpleJob(RunStrategy.ColdStart, iterationCount: 5)]
+    [MemoryDiagnoser]
+    public class BigOBenchmarks
+    {
+        private List<Stock> _stocks;
+        private ImmutableSortedSet<Stock> _sortedStocks;
+        private Dictionary<string, Stock> _dictionaryStocks;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _stocks = Enumerable.Range(0, 100000)
+                .Select(x => new Stock(x.ToString()))
+                .OrderBy(x => Guid.NewGuid())
+                .ToList();
+
+            //_sortedStocks = _stocks
+            //    .OrderBy(x => x)
+            //    .ToImmutableSortedSet();
+        }
+
+        [Benchmark]
+        public Stock? NotSorted()
+        {
+            Stock? element = null;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                element = _stocks.FirstOrDefault(x => x.Ticker == i.ToString());
+            }
+
+            return element;
+        }
+
+        [Benchmark]
+        public Stock? Sorted()
+        {
+            Stock? element = null;
+            _sortedStocks = _stocks
+                .OrderBy(x => x)
+                .ToImmutableSortedSet();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                _sortedStocks.TryGetValue(new(i.ToString()), out element);
+            }
+
+            return element;
+        }
+
+        [Benchmark]
+        public Stock? Dictionary()
+        {
+            Stock? element = null;
+            _dictionaryStocks = _stocks.ToDictionary(x => x.Ticker);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                element = _dictionaryStocks[i.ToString()];
+            }
+
+            return element;
+        }
+    }
+}
--- StockAnalyzer/StockAnalyzer.Processor/Stock.cs
+++ StockAnalyzer/StockAnalyzer.Processor/Stock.cs
@@ -1,10 +1,16 @@
 ﻿namespace StockAnalyzer.Processor;
 
-public class Stock(string ticker)
+public class Stock(string ticker) : IComparable<Stock>
 {
     public IList<Trade> Trades { get; } = new List<Trade>();
     public decimal Min { get; set; } = decimal.MaxValue;
     public decimal Max { get; set; } = decimal.MinValue;
     public decimal Total { get; set; } = 0;
     public decimal Average => Total / Trades.Count;
+    public string Ticker { get; set; } = ticker;
+
+    public int CompareTo(Stock? other)
+    {
+        return string.Compare(Ticker, other.Ticker);
+    }
 }
